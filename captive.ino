c90e7a584b6d8c229761137e90c6cb5cad794e14
Olivia Li <oliviali.ouyang@gmail.com>
2022-01-17 23:11:45 -0800

    new arduino code

--- captive.ino
+++ captive.ino
@@ -1,49 +1,48 @@
 #include <WiFi.h>
-#include <esp_wifi.h>
 
-// Set your new MAC Address
-uint8_t newMACAddress[] = {0x3c, 0x06, 0x30, 0x1c, 0x50, 0xeb};
-const char* ssid     = "BEI_SF";
-const char* password = NULL;
-
+// AP network settings
 const char* soft_ap_ssid = "Olivia-ESP32";
 const char* soft_ap_password = "Chipmunk";
 
-WiFiServer server(80);
+// STA network settings
+const char* ssid     = "BEI_SF";
+const char* password = NULL;
+
+// MAC address variables
+uint8_t newMACAddress[] = {0x3c, 0x06, 0x30, 0x1c, 0x50, 0xeb};
 
 // Setting new ip address
 IPAddress local_IP(172, 20, 7, 121);
 // Set your Gateway IP address
-IPAddress gateway(172, 20, 7, 1);
-IPAddress subnet(255, 255, 255, 0);
+IPAddress gateway(172, 20, 0, 1);
+IPAddress subnet(255, 255, 240, 0);
 
-void setup(){
+// Set web server port number to 80
+WiFiServer server(80);
+
+void setup() {
+
   Serial.begin(115200);
-  Serial.println();
-  
-  // Setting the new MAC Address
+
   Serial.print("[OLD] ESP32 Board MAC Address:  ");
   Serial.println(WiFi.macAddress());
-  esp_wifi_set_mac(WIFI_IF_STA, &newMACAddress[0]);
-  esp_base_mac_addr_set(newMACAddress);
   
+//  esp_wifi_set_mac(WIFI_IF_STA, &newMACAddress[0]);
+  esp_base_mac_addr_set(newMACAddress);
   Serial.print("[NEW] ESP32 Board MAC Address:  ");
   Serial.println(WiFi.macAddress());
 
-  if (!WiFi.config(local_IP, gateway, subnet)) {
-    Serial.println("STA Failed to configure");
-  }
+  WiFi.mode(WIFI_AP_STA);
   
-  WiFi.mode(WIFI_MODE_APSTA);
+  // Setup soft AP 
+  Serial.print("Setting AP (Access Point)…");
   WiFi.softAP(soft_ap_ssid, soft_ap_password);
 
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(".");
+    Serial.println("Connecting to WiFi..");
   }
 
   Serial.print("ESP32 IP as soft AP: ");
@@ -51,10 +50,9 @@ void setup(){
 
   Serial.print("ESP32 IP on the WiFi network: ");
   Serial.println(WiFi.localIP());
-
-  server.begin();
+  
 }
- 
-void loop(){
 
+void loop(){
+ 
 }
